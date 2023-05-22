1fe30d7baf9621991aa0f0473b30194cb6575dad
Michael Campo <mcampo2@gmail.com>
2023-05-22 13:58:45 -0400

    Added a button to the main screen

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
+import { Button } from '@rneui/base';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Button>Button</Button>
+      <Text>{"\n"}</Text>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
