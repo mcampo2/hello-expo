d642cc621a1abe82b11948f0f5bb2f9517bdf7a7
Michael Campo <mcampo2@gmail.com>
2023-05-23 22:33:23 -0400

    Added functionality to Button

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import { Button } from '@rneui/base';
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [isVisible, setIsVisible] = useState(true);
+
+  const toggle  = event => {
+    setIsVisible(!isVisible);
+  };
+
   return (
     <View style={styles.container}>
-      <Button>Button</Button>
+      <Button onPress={toggle}>Button</Button>
       <Text>{"\n"}</Text>
-      <Text>Open up App.js to start working on your app!</Text>
+      { isVisible ? <Text>Open up App.js to start working on your app!</Text> : <Text> </Text> }
       <StatusBar style="auto" />
     </View>
   );
